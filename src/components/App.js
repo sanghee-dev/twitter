4daae4deb2a0143eb03a01eea8dc96f49f98d90b
leeedev <leeesangheee@gmail.com>
2020-12-27 03:09:12 +0900

    Move isLoggedIn to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import AppRouter from "./Router";
 
-const App = () => <AppRouter />;
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+    </>
+  );
+};
 
 export default App;
