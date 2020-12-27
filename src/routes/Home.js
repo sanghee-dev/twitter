ea796eec72a9e841dd4ba5b142cb92dd10ba0595
leeedev <leeesangheee@gmail.com>
2020-12-27 23:32:36 +0900

    Home form SetUp

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Home = () => <div>Home</div>;
+const Home = () => {
+  const [tweet, SetTweet] = useState("");
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  const onChange = (event) => {
+    const { value } = event.target;
+    SetTweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={tweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Tweet" />
+      </form>
+    </div>
+  );
+};
 
 export default Home;
