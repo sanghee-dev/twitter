356817b1799e243fd1a6f31fcc387033cf20a11c
leeedev <leeesangheee@gmail.com>
2020-12-28 02:50:56 +0900

    Getting the Tweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,19 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { dbService } from "fbase";
 
 const Home = () => {
-  const [tweet, SetTweet] = useState("");
+  const [tweet, setTweet] = useState("");
+  const [tweets, setTweets] = useState([]);
+  const getTweets = async () => {
+    const dbTweets = await dbService.collection("tweets").get();
+    dbTweets.forEach((document) => {
+      const tweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setTweets((prev) => [tweetObject, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getTweets();
+  }, []);
   const onSubmit = async (event) => {
     event.preventDefault();
     await dbService.collection("tweets").add({
       tweet,
       createdAt: Date.now(),
     });
-    SetTweet("");
+    setTweet("");
   };
   const onChange = (event) => {
     const { value } = event.target;
-    SetTweet(value);
   };
   return (
     <div>
@@ -27,6 +40,13 @@ const Home = () => {
         />
         <input type="submit" value="Tweet" />
       </form>
+      <div>
+        {tweets.map((tweet) => (
+          <div key={tweet.id}>
+            <span>{tweet.tweet}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
